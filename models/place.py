344cabafc229bb808afea1458d0b8af8552216a9
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-09-19 17:11:45 +0000

    Updated place.py, city.py and user.py

--- models/place.py
+++ models/place.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
-class Place(BaseModel):
-    """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+class Place(BaseModel, Base):
+    """
+    Place class representing the 'places' table.
+
+    Attributes:
+        __tablename__ (str): The table name in the database.
+        city_id (str): The ID of the associated city (foreign key).
+        user_id (str): The ID of the associated user (foreign key).
+        name (str): The name of the place.
+        description (str): A description of the place.
+        number_rooms (int): The number of rooms in the place.
+        number_bathrooms (int): The number of bathrooms in the place.
+        max_guest (int): The maximum number of guests the place can accommodate.
+        price_by_night (int): The price per night for the place.
+        latitude (float): The latitude coordinate of the place.
+        longitude (float): The longitude coordinate of the place.
+    """
+    
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
