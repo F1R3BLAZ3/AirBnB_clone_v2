d5657fc189dc2b2d3218a5942f6bf5daa70e9a18
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-05 15:21:14 +0000

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,37 @@
-#!/usr/bin/python3
+from fabric.api import local
+from datetime import datetime
 """
 script that generates a .tgz archive from the contents of the web_static
 """
 
-from datetime import datetime
-from fabric.api import local
-from os.path import isdir
-
 
 def do_pack():
-    """Compress the files"""
+    """
+    Create a compressed archive of the web_static folder.
+
+    This function generates a timestamp and creates a compressed archive
+    of the web_static folder with a name like "web_static_<timestamp>.tgz".
+    The archive is stored in the "versions" directory.
+
+    Returns:
+        str: The path to the created archive, or None if an error occurs.
+    """
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
+        # Get the current timestamp
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+
+        # Define the archive name
+        archive_name = "web_static_" + timestamp + ".tgz"
+
+        # Create the "versions" directory if it doesn't exist
+        local("mkdir -p versions")
+
+        # Create the compressed archive
+        local("tar -czvf versions/{} web_static".format(archive_name))
+
+        # Return the path to the created archive
+        return "versions/{}".format(archive_name)
+    except Exception:
+        # Return None in case of an error
         return None
