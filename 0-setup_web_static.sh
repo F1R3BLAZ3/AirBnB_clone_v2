c25a9bb6d75daed04b13cc35ea4f63a8fa9e18d6
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-05 15:04:00 +0000

    Task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,40 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static
+# This script sets up the web servers for deployment of web_static.
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
+# Install Nginx if it's not already installed
+if ! [ -x "$(command -v nginx)" ]; then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
 
-sudo apt-get -y install nginx
+# Create necessary directories if they don't exist
+web_static_dir="/data/web_static"
+test_dir="$web_static_dir/releases/test"
+shared_dir="$web_static_dir/shared"
+current_dir="$web_static_dir/current"
 
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p "$web_static_dir" "$test_dir" "$shared_dir"
+sudo touch "$test_dir/index.html"
 
-sudo chown -hR ubuntu:ubuntu /data/
+# Create symbolic link (remove and recreate if it already exists)
+if [ -L "$current_dir" ]; then
+    sudo rm -f "$current_dir"
+fi
+sudo ln -s "$test_dir" "$current_dir"
 
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Give ownership to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu "$web_static_dir"
 
-sudo service nginx start
+# Update Nginx configuration
+config_file="/etc/nginx/sites-available/default"
+config_alias="location /hbnb_static/ {\n    alias $current_dir/;\n}"
+if grep -q "location /hbnb_static/" "$config_file"; then
+    sudo sed -i "s|location /hbnb_static/.*|$config_alias|" "$config_file"
+else
+    sudo sed -i "s|^\s*server {|&\n\n$config_alias|" "$config_file"
+fi
+
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
