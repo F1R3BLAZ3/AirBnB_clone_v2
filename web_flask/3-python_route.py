3414dc25b5177d45b7ea07cf573014f39fb0c94e
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-20 11:39:21 +0000

    Task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -34,6 +34,7 @@ def c_variable(text):
 
 
 @app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 def python_variable(text="is_cool"):
     """
     Display "Python " followed by the value of the text variable.
