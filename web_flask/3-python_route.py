0b8b8e0e5980abab2b825f4327f7d9e7e29e81b8
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-20 11:33:03 +0000

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This is a Flask web application with four routes.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Display "Hello HBNB!" on the root route.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Display "HBNB" on the '/hbnb' route.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_variable(text):
+    """
+    Display "C " followed by the value of the text variable.
+    Replace underscore (_) symbols with a space.
+    """
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_variable(text="is_cool"):
+    """
+    Display "Python " followed by the value of the text variable.
+    Replace underscore (_) symbols with a space.
+    """
+    return "Python " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
