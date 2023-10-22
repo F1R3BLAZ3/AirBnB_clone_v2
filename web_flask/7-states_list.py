9a56c4181f86ecfc999ef3ca8326ca60c352eeb6
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-22 12:49:21 +0000

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application
-"""
-
+"""Start a Flask web application to display states and cities"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def close_session(exception):
+    """Closes the SQLAlchemy session"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a list of all State objects sorted by name"""
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states.values())
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a list of states and their cities"""
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
+
+    return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
