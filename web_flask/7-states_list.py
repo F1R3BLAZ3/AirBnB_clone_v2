9d89329487fcbfa2efab3eddd80a89320efc6e83
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-22 18:34:08 +0000

    Task 8 - added sorting of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,19 +6,20 @@ Starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a list of all State objects sorted by name"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    """Display a list of all State objects sorted by name (A->Z)"""
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """closes the storage on teardown"""
+def teardown_db(exception):
+    """Closes the storage on teardown"""
     storage.close()
 
 
