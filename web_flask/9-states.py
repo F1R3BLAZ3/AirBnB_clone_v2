112bd5c222804998755572f4c4010bc9dc8d7c37
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-22 19:29:35 +0000

    Task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application.
+Starts a Flask web application
 """
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
+def list_states():
+    """Display a list of all State objects sorted by name (A->Z)"""
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    return render_template('9-states.html', states=states)
+
+
 @app.route('/states/<id>', strict_slashes=False)
-def list_states(id=None):
-    """Displays a list of all State objects or
-    City objects linked to a State."""
-    all_states = sorted(storage.all(State).values(),
-                        key=lambda state: state.name)
-
-    if id:
-        state = storage.get(State, id)
-        cities = sorted(state.cities,
-                        key=lambda city: city.name) if state else None
-        return render_template('9-states.html',
-                               selected_state=state, cities=cities)
-
-    return render_template('9-states.html', states=all_states)
+def list_cities_by_state(id):
+    """Display a list of City objects linked to the State with the given ID."""
+    state = storage.get(State, id)
+    if state:
+        cities = sorted(state.cities, key=lambda x: x.name)
+        return render_template('9-states.html', selected_state=state, cities=cities)
+    return render_template('9-states.html')
 
 
 @app.teardown_appcontext
 def close_session(exception):
-    """Closes the current SQLAlchemy session."""
+    """Remove the current SQLAlchemy Session."""
     storage.close()
 
 
