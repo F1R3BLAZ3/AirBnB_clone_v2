8ff79e101ae2150625e4d740d9d83fbf473e827a
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-22 19:58:04 +0000

    Task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,34 +6,28 @@ Starts a Flask web application
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
-app = Flask(__name__)
+app = Flask(__name)
 
 
 @app.route('/states', strict_slashes=False)
-def list_states():
-    """Display a list of all State objects sorted by name (A->Z)"""
-    states = sorted(storage.all(State).values(), key=lambda x: x.name)
-    return render_template('9-states.html', states=states)
+@app.route('/states/<state_id>', strict_slashes=False)
+def list_states_and_cities(state_id=None):
+    """Display the states and cities listed in alphabetical order"""
+    states = storage.all(State)
+    state = None
 
+    if state_id:
+        state = states.get(state_id)
 
-@app.route('/states/<id>', strict_slashes=False)
-def list_cities_by_state(id):
-    """Display a list of City objects linked to the State with the given ID."""
-    state = storage.get(State, id)
-    if state:
-        cities = sorted(state.cities, key=lambda x: x.name)
-        return render_template('9-states.html',
-                               selected_state=state, cities=cities)
-    return render_template('9-states.html')
+    return render_template('9-states.html', states=states.values(), state=state)
 
 
 @app.teardown_appcontext
 def close_session(exception):
-    """Remove the current SQLAlchemy Session."""
+    """Closes the current SQLAlchemy Session."""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
