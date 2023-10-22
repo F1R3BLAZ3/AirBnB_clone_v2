73c642821b63e722dc1d7216a346e90a090d2afe
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-22 20:00:04 +0000

    Task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-app = Flask(__name)
+app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
